29f915198aa76f84a4c0f220909c13d83d1e9fb1
vitoreykirub <vitor.akutsu@rubcube.com>
2024-02-28 16:18:31 -0300

    feat: changed status bar color

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Weather} from './src/lib/weather/screens/weather';
+import {StatusBar} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     // eslint-disable-next-line react/react-in-jsx-scope
     <>
+      <StatusBar backgroundColor="#2a8ee1" />
       <Weather />
     </>
   );
