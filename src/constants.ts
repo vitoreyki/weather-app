914ff0460e478b908c1ddd3306d0ad3e3f7fb0b9
vitoreykirub <vitor.akutsu@rubcube.com>
2024-02-28 17:09:27 -0300

    fix: added https url for constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const apiKey = '3495909d3601492a9d400039242702';
 
 export const currentWeatherApi = (city: string) => {
-  return `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+  return `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
 };
 
 export const searchWeatherApi = (city: string) => {
-  return `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${city}&aqi=no`;
+  return `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${city}&aqi=no`;
 };
